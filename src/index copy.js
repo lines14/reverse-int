dc853cb6a8e9f39acc23ea345260e750d8f88a69
Anton Lipatov <tonybass1414@yahoo.com>
2023-01-18 06:07:39 +0600

    init: start the reverse-int task

---
+++ src/index copy.js
@@ -0,0 +1,21 @@
+function reverse(n) {
+    let str = n.toString();
+    if (str[0] === '-') {
+        str = str.slice(1, str.length);
+        let revNumber = '';
+        for (i in str) {
+            revNumber = `${str[i]}${revNumber}`;
+        };
+        revNumber = Number(revNumber);
+        return revNumber;
+    } else {
+        let revNumber = '';
+        for (i in str) {
+            revNumber = `${str[i]}${revNumber}`;
+        };
+        revNumber = Number(revNumber);
+        return revNumber;
+    };   
+};
+
+console.log(reverse(-234));
