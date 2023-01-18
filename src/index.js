66c5508f78e990108b425163b77317613879a58f
Anton Lipatov <tonybass1414@yahoo.com>
2023-01-18 06:07:56 +0600

    feat: solve the reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = n.toString();
+    if (str[0] === '-') {
+        str = str.slice(1, str.length);
+        let revNumber = '';
+        for (i in str) {
+            revNumber = `${str[i]}${revNumber}`;
+        };
+        revNumber = Number(revNumber);
+        return revNumber;
+    } else {
+        let revNumber = '';
+        for (i in str) {
+            revNumber = `${str[i]}${revNumber}`;
+        };
+        revNumber = Number(revNumber);
+        return revNumber;
+    };   
+};
